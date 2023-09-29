8c79a15af33b203bdb9945e691174db6f0be1bc7
sathvik2002 <hegdesathvik11@gmail.com>
2023-09-29 13:22:06 +0530

    v1.0

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -8,74 +8,74 @@ export default function ToDO() {
   const [inpt, setInpt] = useState<string>("");
   const { incrementBudget } = store();
 
-  const itemsFromLocalStorage = JSON.parse(
-    localStorage.getItem("items") || "[]"
-  );
-  const [arr, setArr] = useState<{ id: string; title: string }[]>(
-    itemsFromLocalStorage
-  );
+  const [arr, setArr] = useState<{ id: string; title: string }[]>();
+  useEffect(() => {
+    setArr(JSON.parse(localStorage.getItem("items") || "[]"));
+  }, []);
 
-  if (typeof window !== "undefined") {
-    useEffect(() => {
-      localStorage.setItem("items", JSON.stringify(arr));
-    }, [arr]);
-  }
+  useEffect(() => {
+    window.localStorage.setItem("items", JSON.stringify(arr));
+  }, [arr]);
 
   return (
     <div className="h-fit min-h-[20%] w-2/3  relative">
       <div className="w-full flex justify-center border border-purple-400 rounded-md bg-purple-400 p-4 text-white font-bold">
         TO DO
       </div>
-      <div
-        className={`w-full flex flex-col items-center mt-4 bg-[#252728] pt-2 ${
-          arr.length > 0
-            ? "border border-[#252728] rounded-tl-md rounded-tr-md"
-            : ""
-        }`}
-      >
-        {arr.map((a) => {
-          return (
-            <div
-              className="w-[70%] bg-[#333536] border border-[#333536] rounded-md mb-1 p-2  flex"
-              key={a.id}
-            >
-              <input
-                type="checkbox"
-                name="yo"
-                id="yo"
-                className="peer mr-2"
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    incrementBudget(5);
-                  } else {
-                    incrementBudget(-5);
-                  }
-                }}
-              />
-              <label
-                htmlFor="yo"
-                className="cursor-pointer text-white peer-checked:line-through peer-checked:text-slate-500"
+      {arr !== undefined ? (
+        <div
+          className={`w-full flex flex-col items-center mt-4 bg-[#252728] pt-2 ${
+            arr.length > 0
+              ? "border border-[#252728] rounded-tl-md rounded-tr-md"
+              : ""
+          }`}
+        >
+          {arr.map((a) => {
+            return (
+              <div
+                className="w-[70%] bg-[#333536] border border-[#333536] rounded-md mb-1 p-2  flex"
+                key={a.id}
               >
-                {a.title}
-              </label>
-              <button
-                className="mr-0 ml-auto text-red-500 hover:font-bold"
-                onClick={() => {
-                  console.log(arr);
-                  setArr(
-                    arr.filter((e) => {
-                      return e.id != a.id;
-                    })
-                  );
-                  console.log(arr);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
-      </div>
+                <input
+                  type="checkbox"
+                  name="yo"
+                  id="yo"
+                  className="peer mr-2"
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      incrementBudget(5);
+                    } else {
+                      incrementBudget(-5);
+                    }
+                  }}
+                />
+                <label
+                  htmlFor="yo"
+                  className="cursor-pointer text-white peer-checked:line-through peer-checked:text-slate-500"
+                >
+                  {a.title}
+                </label>
+                <button
+                  className="mr-0 ml-auto text-red-500 hover:font-bold"
+                  onClick={() => {
+                    console.log(arr);
+                    setArr(
+                      arr.filter((e) => {
+                        return e.id != a.id;
+                      })
+                    );
+                    console.log(arr);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div></div>
+      )}
       <div className=" bg-[#252728] p-3 flex justify-center mt-1 border border-[#252728] rounded-bl-md rounded-br-md focus:outline-none">
         <input
           type="text"
@@ -88,7 +88,7 @@ export default function ToDO() {
         <button
           className="border border-[#414344] rounded-lg font-semibold text-white p-1 hover:text-green-600 hover:border-green-400"
           onClick={() => {
-            setArr((prevArr) => [
+            setArr((prevArr: any) => [
               ...prevArr,
               {
                 id: v4(),
